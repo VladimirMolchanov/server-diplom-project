8ddc89a2c8b3a74755bb2e5ebd3094ed441a5293
Vladimir Molchanov <molchanov.vladimir1993@gmail.com>
2022-04-15 00:09:14 +0300

    We write the logic for registration

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,43 @@
 const express = require('express')
+const User = require('../models/User')
+const TokenService = require('../service/token.service')
+
 const router = express.Router({
     mergeParams: true
 })
 
 router.post('/signUp', async (req, res) => {
+    try {
+        const { email, password } = req.body
 
+        const exitingUser = await User.findOne({ email })
+
+        if (exitingUser) {
+            return res.status(400).json({
+                error: {
+                    message: 'EMAIL_EXISTS'
+                }
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        const newUser = User.create({
+            password: hashedPassword,
+            email,
+            ...req.body
+        })
+
+        const tokens = TokenService.generate({ _id: newUser._id })
+
+        res.status(201).send({
+            ...tokens, userId: newUser._id
+        })
+    } catch (e) {
+        res.status(500).json({
+            message: 'На сервере произошла ошибка. Попробуйте позже'
+        })
+    }
 })
 
 router.post('/signInWithPassword', async (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router({
 
 router.use('/auth', require('./auth.routes'))
 router.use('/category', require('./category.routes'))
-router.use('/color', require('./color.routes'))
+router.use('/colors', require('./color.routes'))
 router.use('/products', require('./products.routes'))
 
 module.exports = router
