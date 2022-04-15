aad51aea07a69d427ff348bd52498f5fc836155b
Vladimir Molchanov <molchanov.vladimir1993@gmail.com>
2022-04-15 16:59:15 +0300

    feat functional product

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+const tokenService = require('../service/token.service')
+
+module.exports = (req, res, next) => {
+    if (req.method === 'OPTIONS') {
+        return next()
+    }
+    
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            })
+        }
+
+        const data = tokenService.validateAccess(token)
+
+        if (!data) {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            })
+        }
+
+        req.user = data
+
+        next()
+    } catch (e) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+}
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,49 +1,75 @@
 const express = require('express')
 const Products = require('../models/Products')
+const auth = require('../middleware/auth.middleware')
+
 const router = express.Router({
     mergeParams: true
 })
 
-router.get('/', async (req, res) => {
-    try {
-        const list = await Products.find();
-        res.status(200).send(list)
-    } catch (e) {
-        res.status(500).json({
-            message: 'На сервере произошла ошибка. Попробуйте позже'
-        })
-        console.log(e)
-    }
-})
-
-router.get('/:productId', async (req, res) => {
-    try {
-        const { productId } = req.params
-
-
-
-    } catch (e) {
-        res.status(500).json({
-            message: 'На сервере произошла ошибка. Попробуйте позже'
-        })
-    }
-})
-
-router.patch('/:productId', async (req, res) => {
-    try {
-        const { productId } = req.params
-
-        if (productId) {
-            const updatedProduct = await Products.findByIdAndUpdate(productId, req.body, {
-                new: true
+router
+    .get('/', async (req, res) => {
+        try {
+            const { orderBy, equalTo } = req.query
+            const list = await Products.find({ [orderBy]: equalTo });
+            res.status(200).send(list)
+        } catch (e) {
+            res.status(500).json({
+                message: 'На сервере произошла ошибка. Попробуйте позже'
             })
-            res.send(updatedProduct)
-        } else {
-            res.status(401).json({
-                message: 'Unauthorized'
+            console.log(e)
+        }
+    })
+    .post('/',auth, async (req, res) => {
+        try {
+            const newProduct = await Products.create({
+                ...req.body,
+            })
+            res.status(201).send(newProduct)
+        } catch (e) {
+            res.status(500).json({
+                message: 'На сервере произошла ошибка. Попробуйте позже'
             })
         }
+    })
+    .get('/:productId', async (req, res) => {
+    try {
+        const { productId } = req.params
+        const product = await Products.findById(productId)
+        res.status(200).json(product)
+    } catch (e) {
+        res.status(500).json({
+            message: 'На сервере произошла ошибка. Попробуйте позже'
+        })
+    }
+})
+    .patch('/:productId', async (req, res) => {
+        try {
+            const { productId } = req.params
 
+            if (productId) {
+                const updatedProduct = await Products.findByIdAndUpdate(productId, req.body, {
+                    new: true
+                })
+                res.send(updatedProduct)
+            } else {
+                res.status(401).json({
+                    message: 'Unauthorized'
+                })
+            }
+
+        } catch (e) {
+            res.status(500).json({
+                message: 'На сервере произошла ошибка. Попробуйте позже'
+            })
+        }
+    })
+    .delete('/:productId', auth, async (req, res) => {
+    try {
+        const { productId } = req.params
+        const removedProduct = await Products.findById(productId)
+
+        await removedProduct.remove()
+        return res.send(null)
     } catch (e) {
         res.status(500).json({
             message: 'На сервере произошла ошибка. Попробуйте позже'
